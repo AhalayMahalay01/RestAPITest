ae27ec2074b7faf8711db69845769c676bafd4f9
Ahalay_Mahalay <artem.levkin13@gmail.com>
2023-08-01 21:12:40 +0300

    Initial version

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,22 +2,49 @@ package main
 
 import (
 	"RestAPITest/internal/config"
-	"fmt"
+	"golang.org/x/exp/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 
 	sfg := config.MustLoad()
-	fmt.Println(sfg)
 
-	// TODO: init config: cleanenv
+	log := setupLogger(sfg.Env)
 
-	// TODO: init logger: slog
+	log.Info("starting url-shortener", slog.String("env", sfg.Env))
+	log.Debug("debug messages are enabled")
 
 	// TODO: init storage: sqlite
 
 	// TODO: init router: chi, "chi render"
 
 	// TODO: run  server:
+
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
 
 }
