3f6f3dce6f050f0ca07e25e2dd9e8d576f8867b4
Ahalay_Mahalay <artem.levkin13@gmail.com>
2023-08-03 16:53:09 +0300

    Initial version

--- internal/storage/sqllite/sqllite.go
+++ internal/storage/sqllite/sqllite.go
@@ -1,9 +1,10 @@
 package sqllite
 
 import (
+	"RestAPITest/internal/storage"
 	"database/sql"
 	"fmt"
-	_ "github.com/mattn/go-sqlite3" // init driver sqlite3
+	"github.com/mattn/go-sqlite3"
 )
 
 type Storage struct {
@@ -34,4 +35,28 @@ func New(storagePath string) (*Storage, error) {
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 	return &Storage{db: db}, nil
+}
+
+func (s *Storage) SaveURL(urlToSave string, alias string) (int64, error) {
+	const op = "storage.sqlite.SaveURL"
+
+	stmt, err := s.db.Prepare("INSERT INTO url(url, alias) VALUES(?, ?)")
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	res, err := stmt.Exec(urlToSave, alias)
+	if err != nil {
+		if sqliteErr, ok := err.(sqlite3.Error); ok && sqliteErr.ExtendedCode == sqlite3.ErrConstraintUnique {
+			return 0, fmt.Errorf("%s: %w", op, storage.ErrURLExists)
+		}
+
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("%s: failed to get last insert id: %w", op, err)
+	}
+
+	return id, nil
 }
